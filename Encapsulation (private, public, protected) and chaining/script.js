943e5ab9d9df62c8723af2beed7ba4079322aef4
LAAJILIC <75376773+LAAJILIC@users.noreply.github.com>
2021-01-21 19:25:26 +0100

    Add files via upload

---
+++ Encapsulation (private, public, protected) and chaining/script.js
@@ -0,0 +1,80 @@
+// Encapsulation: Protected Properties and Methods
+// Encapsulation: Private Class Fields and Methods
+
+class Account {
+  // Public fields (instances)
+  locale = navigator.language;
+
+  // Private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+
+    // Protected property
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public methods
+
+  // Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  requestLoan(val) {
+    // if (this.#approveLoan(val)) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+      return this;
+    }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Cyrine', 'EUR', 1111);
+
+acc1.deposit(450);
+acc1.withdraw(280);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+Account.helper();
+
+//console.log(acc1.#movements); => Error beause it's private
+acc1
+  .deposit(500)
+  .deposit(4500)
+  .deposit(800)
+  .withdraw(1500)
+  .withdraw(200)
+  .deposit(1000)
+  .withdraw(450)
+  .requestLoan(20000);
+console.log(acc1.getMovements());
