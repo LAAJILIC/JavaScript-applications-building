8c9895fd46558d7a96e9e695ab6332ea018561ef
LAAJILIC <75376773+LAAJILIC@users.noreply.github.com>
2021-02-15 22:09:52 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-//contains constant variables that may be used all the project
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'; //(always Uppercase )
-
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 15;
-export const KEY = '1a5fd3dc-d293-4e1c-8545-fe39945abe9c';
-export const MODAL_CLOSE_SEC = 2.5;
